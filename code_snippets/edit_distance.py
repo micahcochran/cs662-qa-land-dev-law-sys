359dc2e2adeb760bae38ac50fd4b112e8599f768
Seth <jesusaur@uab.edu>
2022-11-22 10:35:34 -0600

    code snippets added as reference for xgboost with optuna hyper parameter tuning and levenshin distance with spelling correction, to be added elsewhere in production code

---
+++ code_snippets/edit_distance.py
@@ -0,0 +1,63 @@
+
+import os
+import numpy as np
+import collections
+import ast
+
+file_path = f'{os.getcwd()}'
+
+def min_edit_distance_adjusted(token1, token2, confusion_data):
+    counts = get_sub_counts(token1, token2)
+    distance_matrix = np.zeros((len(token1) + 1, len(token2) + 1))
+
+    for t1 in range(len(token1) + 1):
+        distance_matrix[t1][0] = t1
+
+    for t2 in range(len(token2) + 1):
+        distance_matrix[0][t2] = t2
+        
+    for t1 in range(1, len(token1) + 1):
+        for t2 in range(1, len(token2) + 1):
+            sub_cost = 0
+            del_cost = 1
+            ins_cost = 1
+            if (token1[t1-1] == token2[t2-1]):
+                sub_cost = 0
+            else:
+                sub_cost = generate_spelling_adjusted_sub_cost(token1[t1-1]+token2[t2-1], counts, confusion_data)
+            
+            distance_matrix[t1][t2] = min(distance_matrix[t1 - 1][t2 - 1] + sub_cost, distance_matrix[t1 - 1][t2] + del_cost, 
+                                    distance_matrix[t1][t2 - 1] + ins_cost)
+
+    # print_distance_matrix(distance_matrix, len(token1), len(token2))
+    return distance_matrix[len(token1)][len(token2)]
+
+def get_sub_counts(token1, token2):
+    counts = []
+    for t1 in range(1, len(token1) + 1):
+        for t2 in range(1, len(token2) + 1):
+            if (token1[t1-1] != token2[t2-1]):
+                counts.append(token1[t1-1])
+    counter=collections.Counter(counts)
+    return dict(counter)
+    
+def generate_spelling_adjusted_sub_cost(token1, counts, adjustment_dict):
+    N = counts[token1[0]]
+    x = adjustment_dict[token1]
+    if x == 0:
+        return 0
+    sub_cost = N/x
+    return sub_cost
+
+def print_distance_matrix(distance_matrix, length1, length2):
+    for t1 in range(length1 + 1):
+        for t2 in range(length2 + 1):
+            print(int(distance_matrix[t1][t2]), end=" ")
+        print()
+        
+with open(f'{file_path}/subconfusion.data') as f:
+    data = f.read()
+    
+confusion_data = ast.literal_eval(data) # * pass this to correct common spelling errors
+
+print("Minimum Edit Distance Between", "intention and execution:", min_edit_distance_adjusted("intention", "execution", confusion_data))
